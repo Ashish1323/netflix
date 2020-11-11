bc62ed24a56ec2d46d28bbfaf5f745d9c35980bb
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-11-11 19:46:22 +0530

    Banner Complete

--- my-app/src/Row.js
+++ my-app/src/Row.js
@@ -1,20 +1,25 @@
 import React, {useState,useEffect} from 'react';
 import axios from './axios';
+import requests from './requests';
 import './row.css'
 const base_url = "https://image.tmdb.org/t/p/original"
 const Row = ({ title,fetchUrl,islargerow }) => {
     const [movies, setMovies] = useState([]);
 
-        useEffect(() => {
-            async function fetchData() {
-                const request = await axios.get(fetchUrl);
-                console.log(typeof(request))
-                setMovies(request.data.results);
-                
-                return request;
-            }
-            fetchData()
-        }, [fetchUrl]);
+       
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            
+            setMovies(request.data.results);
+            
+            return request;
+        }
+        fetchData()
+    }, [fetchUrl]);
+
+
         return (
         <div className="Row">
             {title}
